db17b830bda9b9681b556a4da48a3a438de293f6
vaishnavimulik25 <vaishnavimulik25@gmail.com>
2023-05-22 22:24:41 +0530

    status

--- main.py
+++ main.py
@@ -49,24 +49,24 @@ headingLabel = Label(headingFrame1, text="Welcome to \n COEP BOOK STORE", bg='bl
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
 headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-headingFrame1.place(relx=0.2,rely=0.2,relwidth=0.6,relheight=0.12)
+headingFrame1.place(relx=0.2,rely=0.28,relwidth=0.6,relheight=0.10)
 
 headingLabel = Label(headingFrame1, text="The only true equalizers in the world are books\nThe only treasure house open to all comers is a library\nThe only wealth which will not decay is wisdom", bg='yellow', fg='black', font=('Courier',25))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
 btn1 = Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
-btn1.place(relx=0.28,rely=4.00, relwidth=0.45,relheight=0.1)
+btn1.place(relx=0.28,rely=0.4, relwidth=0.45,relheight=0.1)
     
 btn2 = Button(root,text="Delete Book",bg='black', fg='white', command=delete)
-btn2.place(relx=0.28,rely=5.00, relwidth=0.45,relheight=0.1)
+btn2.place(relx=0.28,rely=0.5, relwidth=0.45,relheight=0.1)
     
 btn3 = Button(root,text="View Book List",bg='black', fg='white', command=View)
-btn3.place(relx=0.28,rely=6.00, relwidth=0.45,relheight=0.1)
+btn3.place(relx=0.28,rely=0.6, relwidth=0.45,relheight=0.1)
     
 btn4 = Button(root,text="Issue Book to Student",bg='black', fg='white', command = issueBook)
-btn4.place(relx=0.28,rely=7.00, relwidth=0.45,relheight=0.1)
+btn4.place(relx=0.28,rely=0.7, relwidth=0.45,relheight=0.1)
     
 btn5 = Button(root,text="Return Book",bg='black', fg='white', command = returnBook)
-btn5.place(relx=0.28,rely=8.00, relwidth=0.45,relheight=0.1)
+btn5.place(relx=0.28,rely=0.8, relwidth=0.45,relheight=0.1)
 
 root.mainloop()
